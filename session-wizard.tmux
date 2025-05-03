b2453fcd2881d1e3a986e122f85fdd69f20c26d3
Simon Martens <simon.martens@mailbox.org>
2025-05-03 10:55:19 +0200

    Split window, not popup

--- session-wizard.tmux
+++ session-wizard.tmux
@@ -28,7 +28,8 @@ set_session_wizard_options() {
   width=$(get_tmux_option "$tmux_option_session_wizard_width" "$default_width")
   local key
   for key in $(echo "${key_bindings}" | sed 's/ /\n/g'); do
-    tmux bind "$key" display-popup -w "$width"% -h "$height"% -E "$CURRENT_DIR/bin/t"
+    # tmux bind "$key" display-popup -w "$width"% -h "$height"% -E "$CURRENT_DIR/bin/t"
+    tmux bind t split-window -v "$CURRENT_DIR/bin/t"
   done
 }
 
--- src/helpers.sh
+++ src/helpers.sh
@@ -60,6 +60,9 @@ if [ $HOME_SED_SAFE -eq 0 ]; then # $HOME should be safe to use in sed
 fi
 
 __fzfcmd() {
-  [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; } &&
-    echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- " || echo "fzf"
+  if [ -n "$TMUX_PANE" ] && { [ "${FZF_TMUX:-0}" != 0 ] || [ -n "$FZF_TMUX_OPTS" ]; }; then
+      echo "fzf-tmux ${FZF_TMUX_OPTS:--d${FZF_TMUX_HEIGHT:-40%}} -- "
+    else 
+      echo "fzf"
+    fi
 }
