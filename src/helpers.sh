667048a785c0bbd4ed45155d9d897b4fbb4a767a
Simon Martens <simon.martens@mailbox.org>
2025-05-03 18:49:39 +0200

    Further simpliefied the script

--- src/helpers.sh
+++ src/helpers.sh
@@ -2,14 +2,6 @@ _normalize() {
   cat | tr ' .:' '-' | tr '[:upper:]' '[:lower:]'
 }
 
-function clean_up() {
-  # Clean up tmp session
-  if [[ -n "$TMP_SESSION_NAME" ]]; then
-    tmux kill-session -t "$TMP_SESSION_NAME" 2>/dev/null
-    rm -rf "$TMP_SESSION_DIR"
-  fi
-}
-
 # helper functions
 get_tmux_option() {
   local option="$1"
@@ -31,22 +23,6 @@ set_tmux_option() {
   option_value=$(tmux show-option -gqv "$option")
   if [ -z "$option_value" ]; then
     tmux set-option -g "$option" "$default_value"
-  fi
-}
-
-session_name() {
-  if [ "$1" = "--directory" ]; then
-    shift
-    basename "$@" | _normalize
-  elif [ "$1" = "--full-path" ]; then
-    shift
-    echo "$@" | _normalize | sed 's/\/$//'
-  elif [ "$1" = "--short-path" ]; then
-    shift
-    echo "$(echo "${@%/*}" | sed -r 's;/([^/]{1,2})[^/]*;/\1;g' | _normalize)/$(basename "$@" | _normalize)"
-  else
-    echo "Wrong argument, you can use --directory, --full-path or --short-path, got $1"
-    return 1
   fi
 }
 
